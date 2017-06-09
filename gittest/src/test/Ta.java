e76ec0876f550e041c109001805f3f91c36ec14d
Administrator <Administrator@84LQ7TPLZN405VE>
2017-06-09 11:53:48 +0800

    1212

--- gittest/src/test/Ta.java
+++ gittest/src/test/Ta.java
@@ -1,11 +1,12 @@
 package test;
 
-public class ta {
+public class Ta {
 	public int sum(int x,int y){
 		return x+y;
 	}
-	public void print(){}
+	public void print(int x){}
 	public static void main(String[] args) {
-		
+		Ta ata=new Ta(); 
+		ata.print(3);
 	}
 }
